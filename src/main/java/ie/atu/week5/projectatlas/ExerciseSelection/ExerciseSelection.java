a89eb5b1dc72d86680d9cc5437cc9da586ca0e61
c0nor-S <150271462+c0nor-S@users.noreply.github.com>
2026-02-28 16:53:45 +0000

    Fixed Exercise Selection API: Supports MuscleGroups Query And Returns Filtered Exercises.

--- src/main/java/ie/atu/week5/projectatlas/ExerciseSelection/ExerciseSelection.java
+++ src/main/java/ie/atu/week5/projectatlas/ExerciseSelection/ExerciseSelection.java
@@ -3,23 +3,38 @@ package ie.atu.week5.projectatlas.ExerciseSelection;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.List;
 
-@Getter
-@Setter
 public class ExerciseSelection {
     @Schema(accessMode = Schema.AccessMode.READ_ONLY)
     private Long muscleGroupID;
-
-    public void setMuscleGroupID(Long muscleGroupID) {
-        this.muscleGroupID = muscleGroupID;
-    }
 
     @NotBlank(message = "Muscle Group Is Required")
     private String muscleGroup;
 
     private List<String> exercises;
+
+    public Long getMuscleGroupID() {
+        return muscleGroupID;
+    }
+
+    public void setMuscleGroupID(Long muscleGroupID) {
+        this.muscleGroupID = muscleGroupID;
+    }
+
+    public String getMuscleGroup() {
+        return muscleGroup;
+    }
+
+    public void setMuscleGroup(String muscleGroup) {
+        this.muscleGroup = muscleGroup;
+    }
+
+    public List<String> getExercises() {
+        return exercises;
+    }
+
+    public void setExercises(List<String> exercises) {
+        this.exercises = exercises;
+    }
 }
--- src/main/java/ie/atu/week5/projectatlas/ExerciseSelectionService/ExerciseSelectionService.java
+++ src/main/java/ie/atu/week5/projectatlas/ExerciseSelectionService/ExerciseSelectionService.java
@@ -16,17 +16,24 @@ public class ExerciseSelectionService {
         chest.setMuscleGroupID(nextMuscleGroupID++);
         chest.setMuscleGroup("Chest");
         chest.setExercises(List.of("Flat Bench Press", "Incline Bench Press", "Chest Fly", "Push Up"));
-
         exerciseSelections.add(chest);
+
+        ExerciseSelection back = new ExerciseSelection();
+        back.setMuscleGroupID(nextMuscleGroupID++);
+        back.setMuscleGroup("Back");
+        back.setExercises(List.of("Pull Up", "Lat Pulldown", "Deadlift", "T-Bar Row"));
+        exerciseSelections.add(back);
     }
-    public ExerciseSelection addExerciseSelection(ExerciseSelection exerciseSelection)
-    {
-        exerciseSelection.setMuscleGroupID(nextMuscleGroupID++);
-        exerciseSelections.add(exerciseSelection);
-        return exerciseSelection;
-    }
+
     public List<ExerciseSelection> getAllExerciseSelections()
     {
-        return exerciseSelections;
+        return new ArrayList<>(exerciseSelections);
+    }
+
+    public ExerciseSelection getExercisesByMuscleGroup(String muscleGroup) {
+        return exerciseSelections.stream()
+                .filter(e -> e.getMuscleGroup().equals(muscleGroup))
+                .findFirst()
+                .orElse(null);
     }
 }
